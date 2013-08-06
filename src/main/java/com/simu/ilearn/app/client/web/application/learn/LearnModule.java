3dcfcdffd755c22b22a004f62c672bdab64bb243
Kecha Mohamed <kecha.mohamed@gmail.com>
2013-08-06 10:28:15 +0000

    Root presenter and View Learn

---
+++ src/main/java/com/simu/ilearn/app/client/web/application/learn/LearnModule.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2012 Nuvola Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.simu.ilearn.app.client.web.application.learn;
+
+import com.google.gwt.inject.client.assistedinject.GinFactoryModuleBuilder;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+import com.simu.ilearn.app.client.web.application.learn.widget.LearnWidgetFactory;
+import com.simu.ilearn.app.client.web.application.learn.widget.LearnWidgetPresenter;
+import com.simu.ilearn.app.client.web.application.learn.widget.LearnWidgetView;
+
+public class LearnModule extends AbstractPresenterModule {
+    @Override
+    protected void configure() {
+        bind(LearnUiHandlers.class).to(LearnPresenter.class);
+
+        bindPresenter(LearnPresenter.class, LearnPresenter.MyView.class, LearnView.class,
+                LearnPresenter.MyProxy.class);
+        bindSharedView(LearnWidgetPresenter.MyView.class, LearnWidgetView.class);
+
+        install(new GinFactoryModuleBuilder().build(LearnWidgetFactory.class));
+    }
+}
